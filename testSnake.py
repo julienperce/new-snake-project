97aaa2f73bf0959962d01a88b0d5c2e85346e48f
Julien Perce <58319391+julienperce@users.noreply.github.com>
2021-04-29 20:31:58 -0500

    update
    spawns 2 block of color, color in rainbow order

--- testSnake.py
+++ testSnake.py
@@ -5,11 +5,20 @@ import random
 # https://github.com/kiteco/python-youtube-code/blob/master/snake/snake.py 
 
 #GLOBAL CONSTANTS
-SCREEN_WIDTH = 480
-SCREEN_HEIGHT = 480
+SCREEN_WIDTH = 720
+SCREEN_HEIGHT = 720
 GRIDSIZE = 20
 GRID_WIDTH = SCREEN_HEIGHT / GRIDSIZE
 GRID_HEIGHT = SCREEN_WIDTH / GRIDSIZE
+
+# COLOR CONSTANTS
+RED = (255, 0, 0)
+ORANGE = (255, 127, 0)
+YELLOW = (255, 255, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+INDIGO = (75, 0, 130)
+VIOLET = (148, 0, 211)
 
 #LEGAL MOVES FOR THE SNAKE
 UP = (0, -1)
@@ -20,11 +29,13 @@ RIGHT = (1, 0)
 running = True
 
 class Snake(object):
+    currentColor = RED
+    
     def __init__(self):
         self.length = 1
         self.positions = [((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))] # list of all positions of each block that snake is made of
         self.direction = random.choice([UP, DOWN, LEFT, RIGHT])
-        self.color = (17, 24, 47)
+        self.color = RED
     
     def get_head_position(self):
         return self.positions[0]
@@ -55,14 +66,13 @@ class Snake(object):
         self.length = 1
         self.positions = [((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))]
         self.direction = random.choice([UP, DOWN, LEFT, RIGHT])
-        running = False
 
     def draw(self, surface):
         # draw a block for each x, y block in the snake's positions list
         for pos in self.positions:
             r = pygame.Rect((pos[0], pos[1]), (GRIDSIZE, GRIDSIZE))
-            pygame.draw.rect(surface, self.color, r)
-            pygame.draw.rect(surface, (93, 216, 228), r, 1)
+            pygame.draw.rect(surface, self.currentColor, r)
+            pygame.draw.rect(surface, (255, 255, 255), r, 1)
     
     def handle_keys(self):
         # KEY EVENT HANDLER
@@ -73,29 +83,35 @@ class Snake(object):
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
                 # yeah ion needa comment i think you know what's going on here
-                if event.key == pygame.K_UP:
+                if event.key == pygame.K_UP or event.key == pygame.K_w:
                     self.turn(UP)
-                elif event.key == pygame.K_DOWN:
+                elif event.key == pygame.K_DOWN or event.key == pygame.K_s:
                     self.turn(DOWN)
-                elif event.key == pygame.K_LEFT:
+                elif event.key == pygame.K_LEFT or event.key == pygame.K_a:
                     self.turn(LEFT)
-                elif event.key == pygame.K_RIGHT:
+                elif event.key == pygame.K_RIGHT or event.key == pygame.K_d:
                     self.turn(RIGHT)
 
 
 class Food(object):
+    foodCurrentColor = ORANGE
+    
     def __init__(self):
         self.position = (0, 0)
-        self.color = (223, 163, 49)
+        self.color = ORANGE
         self.randomize_position()
 
     def randomize_position(self):
-        self.position = (random.randint(0, GRID_HEIGHT-1)*GRIDSIZE, random.randint(0, GRID_HEIGHT-1)*GRIDSIZE)
+        self.position1 = (random.randint(0, GRID_HEIGHT-1)*GRIDSIZE, random.randint(0, GRID_HEIGHT-1)*GRIDSIZE)
+        self.position2 = (random.randint(0, GRID_HEIGHT-1)*GRIDSIZE, random.randint(0, GRID_HEIGHT-1)*GRIDSIZE)
 
     def draw(self, surface):
-        r = pygame.Rect((self.position[0], self.position[1]), (GRIDSIZE, GRIDSIZE))
-        pygame.draw.rect(surface, self.color, r)
-        pygame.draw.rect(surface, (93, 216, 228), r, 1)
+        r1 = pygame.Rect((self.position1[0], self.position1[1]), (GRIDSIZE, GRIDSIZE))
+        pygame.draw.rect(surface, self.foodCurrentColor, r1)
+        pygame.draw.rect(surface, (0, 0, 0), r1, 1)
+        r2 = pygame.Rect((self.position2[0], self.position2[1]), (GRIDSIZE, GRIDSIZE))
+        pygame.draw.rect(surface, self.foodCurrentColor, r2)
+        pygame.draw.rect(surface, (0, 0, 0), r2, 1)
 
 def drawGrid(surface):
     for y in range(0, int(GRID_HEIGHT)):
@@ -109,7 +125,6 @@ def drawGrid(surface):
                 pygame.draw.rect(surface, (64, 64, 64), r2)
 
 def main():
-    pygame.init()
 
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
@@ -125,22 +140,75 @@ def main():
      
     score = 0
 
-    FONT = pygame.font.SysFont("monospace", 16)
+    FONT = pygame.font.SysFont("arial", 16, bold=True)
 
-    while running:
-        # increment our clock at 10 FPS
+    pygame.display.set_caption("SNAKE!")
+
+    while True:
+        # increment our clock at x FPS
         clock.tick(10)
         snake.handle_keys()
         # fill background
         drawGrid(surface)
         
         snake.move()
-        if snake.get_head_position() == food.position:
-            # if snake head is on same x, y as a food it has eaten it 
-            snake.length += 1
-            score += 1
-            # randomize x, y of next food bloock
-            food.randomize_position()
+        if snake.get_head_position() == food.position1 or snake.get_head_position() == food.position2:
+            if snake.currentColor == RED:
+                # if snake head is on same x, y as a food it has eaten it 
+                snake.currentColor = ORANGE
+                food.foodCurrentColor = YELLOW
+                snake.length += 1
+                score += 1
+                # randomize x, y of next food bloock
+                food.randomize_position()
+            elif snake.currentColor == ORANGE:
+                # if snake head is on same x, y as a food it has eaten it 
+                snake.currentColor = YELLOW
+                food.foodCurrentColor = GREEN
+                snake.length += 1
+                score += 1
+                # randomize x, y of next food bloock
+                food.randomize_position()
+            elif snake.currentColor == YELLOW:
+                # if snake head is on same x, y as a food it has eaten it 
+                snake.currentColor = GREEN
+                food.foodCurrentColor = BLUE
+                snake.length += 1
+                score += 1
+                # randomize x, y of next food bloock
+                food.randomize_position()
+            elif snake.currentColor == GREEN:
+                # if snake head is on same x, y as a food it has eaten it 
+                snake.currentColor = BLUE
+                food.foodCurrentColor = INDIGO
+                snake.length += 1
+                score += 1
+                # randomize x, y of next food bloock
+                food.randomize_position()
+            elif snake.currentColor == BLUE:
+                # if snake head is on same x, y as a food it has eaten it 
+                snake.currentColor = INDIGO
+                food.foodCurrentColor = VIOLET
+                snake.length += 1
+                score += 1
+                # randomize x, y of next food bloock
+                food.randomize_position()
+            elif snake.currentColor == INDIGO:
+                # if snake head is on same x, y as a food it has eaten it 
+                snake.currentColor = VIOLET
+                food.foodCurrentColor = RED
+                snake.length += 1
+                score += 1
+                # randomize x, y of next food bloock
+                food.randomize_position()
+            elif snake.currentColor == VIOLET:
+                # if snake head is on same x, y as a food it has eaten it 
+                snake.currentColor = RED
+                food.foodCurrentColor = ORANGE
+                snake.length += 1
+                score += 1
+                # randomize x, y of next food bloock
+                food.randomize_position()
         
         # before updating our screen/frame, we need to redraw the snake and food based on their new positions
         snake.draw(surface)
@@ -150,10 +218,12 @@ def main():
         screen.blit(surface, (0, 0))
         
         # display and update our score
-        scoreDisplay = FONT.render("SCORE : {}".format(score), 1, (0, 0, 0))
+        scoreDisplay = FONT.render("SCORE : {}".format(score), 1, (255, 255, 255))
         screen.blit(scoreDisplay, (5, 10))
-        
+
         pygame.display.update()
         
-
-main()
+    
+if __name__ == "__main__":
+    pygame.init()
+    main()
